bbbb248d9ae5c3f95df32c329e0f5d0436ef5c21
sheri <sheri_situ@163.com>
2023-10-17 14:39:02 +0800

    feat: baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-blog',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -128,6 +128,7 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+ 
 };
 
 module.exports = config;
